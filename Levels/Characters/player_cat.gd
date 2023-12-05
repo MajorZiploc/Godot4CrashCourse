c5b67117bbf6618b9ce4a8e233d2b249b1ff4c7d
Manyu Lakhotia <lakemonyew@gmail.com>
2023-12-04 18:15:32 -0600

    mob_cow

--- Levels/Characters/player_cat.gd
+++ Levels/Characters/player_cat.gd
@@ -4,8 +4,7 @@ extends CharacterBody2D
 @export var move_speed: float = 100;
 @export var starting_direction: Vector2 = Vector2(0, 1);
 
-@onready var animation_tree = $AnimationTree;
-@onready var state_machine = animation_tree.get("parameters/playback");
+@onready var state_machine = $AnimationTree.get("parameters/playback");
 
 func _ready():
 	update_animation_parameters(starting_direction);
@@ -23,8 +22,8 @@ func _physics_process(_delta):
 	
 func update_animation_parameters(move_input: Vector2):
 	if (move_input != Vector2.ZERO):
-		animation_tree.set("parameters/Walk/blend_position", move_input)
-		animation_tree.set("parameters/Idle/blend_position", move_input)
+		$AnimationTree.set("parameters/Walk/blend_position", move_input)
+		$AnimationTree.set("parameters/Idle/blend_position", move_input)
 		
 func pick_new_state():
 	if (velocity != Vector2.ZERO):
