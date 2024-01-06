a6bf9cc70c36f0e1aafdfbdf6a8251791d9a40d6
Manyu Lakhotia <lakemonyew@gmail.com>
2024-01-06 10:27:10 -0600

    +change_colors script;+global utils examples;to 2space instead of hard tabs

--- Levels/Characters/mob_cow.gd
+++ Levels/Characters/mob_cow.gd
@@ -12,32 +12,32 @@ var move_direction: Vector2 = Vector2.ZERO;
 var current_state: COW_STATE = COW_STATE.IDLE;
 
 func _ready():
-	pick_new_state();
+  pick_new_state();
 
 func _physics_process(delta):
-	if (current_state != COW_STATE.WALK): return
-	velocity = move_direction * move_speed;
-	move_and_slide();
-	
+  if (current_state != COW_STATE.WALK): return
+  velocity = move_direction * move_speed;
+  move_and_slide();
+  
 func select_new_direction():
-	move_direction = Vector2(
-		randi_range(-1, 1)
-		, randi_range(-1, 1)
-	);
-	if (move_direction.x != 0):
-		$Sprite2D.flip_h = move_direction.x < 0;
+  move_direction = Vector2(
+    randi_range(-1, 1)
+    , randi_range(-1, 1)
+  );
+  if (move_direction.x != 0):
+    $Sprite2D.flip_h = move_direction.x < 0;
 
 func pick_new_state():
-	if (current_state == COW_STATE.IDLE):
-		state_machine.travel("walk_right");
-		current_state = COW_STATE.WALK;
-		select_new_direction();
-		$Timer.start(walk_time);
-	elif (current_state == COW_STATE.WALK):
-		state_machine.travel("idle_right");
-		current_state = COW_STATE.IDLE;
-		select_new_direction();
-		$Timer.start(idle_time);
+  if (current_state == COW_STATE.IDLE):
+    state_machine.travel("walk_right");
+    current_state = COW_STATE.WALK;
+    select_new_direction();
+    $Timer.start(walk_time);
+  elif (current_state == COW_STATE.WALK):
+    state_machine.travel("idle_right");
+    current_state = COW_STATE.IDLE;
+    select_new_direction();
+    $Timer.start(idle_time);
 
 func _on_timer_timeout():
-	pick_new_state();
+  pick_new_state();
